b3a39d19bd0fb161c9c096be3da78cf5f882ee2d
Kristian Tashkov <kristian.tashkov@gmail.com>
2015-03-22 00:59:13 +0200

    Fix logout

--- controllers/user_controller.rb
+++ controllers/user_controller.rb
@@ -3,34 +3,6 @@
 class UserController < ApplicationController
   NAMESPACE = '/user/'
   
-  get '/:id' do
-    @title = "User information"
-    @user = User.find(id: params[:id]);
-    
-    @rewardsDonated = 0
-    ratingsSum = 0;
-    
-    completedApplications = Application.where(user: @user, status: 3).all
-    completedApplications.each do |application|
-      @rewardsDonated += (application.task.reward)*(application.share)
-      ratingsSum += application.rating
-    end
-    
-    @completedTasksCount = completedApplications.size
-    if @completedTasksCount == 0
-      @avgRating = "Not available"
-    else
-      @avgRating = ratingsSum/@completedTasksCount
-    end
-    
-    @recentlyCompleted = []
-    completedApplications.last(PROFILE_RECENTLY_COMPLETED_COUNT).each do |recentTask|
-      recentlyCompleted.push(recentTask.title)
-    end
-  
-    erb :profile
-  end
-
   post '/register' do
     username = params[:username]
     password = params[:password]
@@ -74,6 +46,34 @@ class UserController < ApplicationController
 
   get '/logout' do
     logout_user
+  end
+
+   get '/:id' do
+    @title = "User information"
+    @user = User.find(id: params[:id]);
+    
+    @rewardsDonated = 0
+    ratingsSum = 0;
+    
+    completedApplications = Application.where(user: @user, status: 3).all
+    completedApplications.each do |application|
+      @rewardsDonated += (application.task.reward)*(application.share)
+      ratingsSum += application.rating
+    end
+    
+    @completedTasksCount = completedApplications.size
+    if @completedTasksCount == 0
+      @avgRating = "Not available"
+    else
+      @avgRating = ratingsSum/@completedTasksCount
+    end
+    
+    @recentlyCompleted = []
+    completedApplications.last(PROFILE_RECENTLY_COMPLETED_COUNT).each do |recentTask|
+      recentlyCompleted.push(recentTask.title)
+    end
+  
+    erb :profile
   end
 end
 
