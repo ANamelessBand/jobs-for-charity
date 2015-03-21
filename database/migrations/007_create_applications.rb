eee520ba71db6f28a6718305de1a198334bed5dc
dakatapetrov <daka7a@gmail.com>
2015-03-21 18:47:41 +0200

    Add applications table migration

---
+++ database/migrations/007_create_applications.rb
@@ -0,0 +1,14 @@
+Sequel.migration do
+  change do
+    create_table(:applciations) do
+      primary_key :id, index: true
+      foreign_key :taks_id, :tasks
+      foreign_key :appliacnt_id, :users
+      foreign_key :charity_id. :charities
+      Decimal :share, precision: 2, null: false
+      Integer :status, null: false, default: 0
+      String :motivation, null: false
+      Integer :rating, null: false, default: 0
+    end
+  end
+end
