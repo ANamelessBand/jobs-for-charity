5a2c8b2c39908942644428b5cc0583d8e83269ed
jorenca <jv_mw@abv.bg>
2015-03-21 23:20:54 +0200

    profile page import.

--- controllers/profile_controller.rb
+++ controllers/profile_controller.rb
@@ -2,8 +2,25 @@ class ProfileController < ApplicationController
   NAMESPACE = '/profile'
   
   get '/:id' do
-	@user = User.find(id: params[:id]);
 	@title = "zomg"
+	@user = User.find(id: params[:id]);
+	
+	@rewards = 0
+	ratingsSum = 0;
+	
+	completedApplications = Application.where(user: @user, status: 3).all
+	completedApplications.each do |application|
+	  @rewards += (application.task.reward)*(application.share)
+	  ratingsSum += application.rating
+	end
+	
+	@completedTasksCount = completedApplications.size
+	if @completedTasksCount == 0
+	  @avgRating = "Not available"
+	else
+	  @avgRating = ratingsSum/@completedTasksCount
+	end
+	
     erb :profile
   end
 
