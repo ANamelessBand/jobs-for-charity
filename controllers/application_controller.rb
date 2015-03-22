ca58221bcb86511ba075483ffc0b1efafc0b4272
Kristian Tashkov <kristian.tashkov@gmail.com>
2015-03-22 09:37:50 +0200

    Add charity navigation link

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < Sinatra::Base
     @navigation_links = [
     					  NavigationLink.new(NavigationLink.home_id, '/index', 'Home'),
                           NavigationLink.new(NavigationLink.dashboard_id, '/dashboard', 'Dashboard'),
-                          NavigationLink.new(NavigationLink.top_users_id, '/user/top', 'Top Users')
+                          NavigationLink.new(NavigationLink.top_users_id, '/user/top', 'Top Users'),
+                          NavigationLink.new(NavigationLink.add_charity_id, '/charity/add', 'Add Charity'),
                         ]
   end
 end
